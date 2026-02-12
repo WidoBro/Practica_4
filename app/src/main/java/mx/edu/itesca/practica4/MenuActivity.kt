520086a9672bae38fbd568d98d22a70bf044cfc4
Andres Huidobro <ahuidobro66@gmail.com>
2026-02-12 08:52:04 -0700

    Front practica 4

---
+++ app/src/main/java/mx/edu/itesca/practica4/MenuActivity.kt
@@ -0,0 +1,19 @@
+package mx.edu.itesca.practica4
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+
+    }
+}
